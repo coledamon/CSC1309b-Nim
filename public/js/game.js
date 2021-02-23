9a1eebc1b33824eb890586dd1e7404a60716ba03
Brandon Ortiz <bortiz@student.neumont.edu>
2021-02-22 17:51:19 -0700

    check if no more pieces are present then display end screen

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,11 @@
-let pieces_container = document.getElementById("pieces");
+let pieces_container = document.getElementById("pieces-container");
 
 populateAndRenderPieces = () => {
     
     //Add a piece 21 times
     for(i = 0; i < 21; i++){
         let piece = document.createElement("span");
-        piece.className = "game_piece";
+        piece.className = "game-piece";
         pieces_container.appendChild(piece);
     };
         
@@ -15,7 +15,15 @@ takePiece = (amount) => {
     //console.log(pieces_container.firstElementChild);
     for(i = 0; i < amount; i++) pieces_container.firstChild.remove();
     
-    //probably call like other stuff here (like checkIfStack is 0)
+    //probably call like other stuff here (like checkIfStack is 0 and change turn)
+    checkForWin();
+};
+
+checkForWin = () => {
+    if(pieces_container.childElementCount == 0){
+        console.log("game is over!");
+        document.getElementById("end-modal").style.display = "block";
+    }
 };
 
 window.onload = function(){
