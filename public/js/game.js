78e97b6a36ce4818da9bf926d914d10488b83ae0
Brandon Ortiz <bortiz@student.neumont.edu>
2021-02-22 17:19:06 -0700

    Give take piece buttons functionality

--- public/js/game.js
+++ public/js/game.js
@@ -1,21 +1,21 @@
+let pieces_container = document.getElementById("pieces");
 
 populateAndRenderPieces = () => {
-    let pieces_container = document.getElementById("pieces");
-
+    
     //Add a piece 21 times
     for(i = 0; i < 21; i++){
         let piece = document.createElement("span");
         piece.className = "game_piece";
-
-        //Remove this style stuff later (*for now just to see pieces*)
-        piece.style.width = "50px";
-        piece.style.height = "50px";
-        piece.style.border = "2px solid black";
-        piece.style.borderRadius = "25px";
-
         pieces_container.appendChild(piece);
     };
         
+};
+
+takePiece = (amount) => {
+    //console.log(pieces_container.firstElementChild);
+    for(i = 0; i < amount; i++) pieces_container.firstChild.remove();
+    
+    //probably call like other stuff here (like checkIfStack is 0)
 };
 
 window.onload = function(){
